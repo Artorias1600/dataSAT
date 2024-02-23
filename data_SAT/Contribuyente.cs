b12cdb9b6b1f2173260d24862743c4c8a203c515
Luis Morales <luis.moraless@elektra.com.mx>
2024-02-23 17:43:14 -0600

    version 1.0 data_sat

---
+++ data_SAT/Contribuyente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Contribuyente
+{
+    public string Nit { get; set; }
+    public string Nombre { get; set; }
+    public string DireccionFiscal { get; set; }
+    public string Telefono { get; set; }
+    public string CorreoElectronico { get; set; }
+
+    public Contribuyente(string nit, string nombre, string direccionFiscal, string telefono, string correoElectronico)
+    {
+        Nit = nit;
+        Nombre = nombre;
+        DireccionFiscal = direccionFiscal;
+        Telefono = telefono;
+        CorreoElectronico = correoElectronico;
+    }
+
+    public void ImprimirDatos()
+    {
+        Console.WriteLine($"Nit: {Nit}, Nombre: {Nombre}, Teléfono: {Telefono}, Correo Electrónico: {CorreoElectronico}");
+    }
+}
---
+++ data_SAT/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Contribuyente contribuyente1 = new Contribuyente("1234567-8", "Juan Perez", "1ra Calle, Zona 1", "12345678", "juanperez@example.com");
+        Contribuyente contribuyente2 = new Contribuyente("2345678-9", "Maria Lopez", "2da Avenida, Zona 2", "23456789", "marialopez@example.com");
+        Contribuyente contribuyente3 = new Contribuyente("3456789-0", "Carlos Ramirez", "3ra Callejón, Zona 3", "34567890", "carlosramirez@example.com");
+        Contribuyente contribuyente4 = new Contribuyente("4567890-1", "Ana Fernández", "4ta Avenida, Zona 4", "45678901", "anafernandez@example.com");
+        Contribuyente contribuyente5 = new Contribuyente("5678901-2", "Luis Méndez", "5ta Calle, Zona 5", "56789012", "luismendez@example.com");
+
+        contribuyente1.ImprimirDatos();
+        contribuyente2.ImprimirDatos();
+        contribuyente3.ImprimirDatos();
+        contribuyente4.ImprimirDatos();
+        contribuyente5.ImprimirDatos();
+    }
+}
